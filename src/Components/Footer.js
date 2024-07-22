9493ffd10c9d18a39c635675dd5317e7167bc333
Sagar Sharma <sagarsharma1832@gmail.com>
2024-07-22 15:27:30 +0530

    new commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Col, Row, Divider } from "antd";
+import { useNavigate } from "react-router-dom";
 function Footer() {
+const navigate = useNavigate();
+  function handleClick(z){
+  if (z==="Home")navigate('/')
+    else if(z==="Shop")navigate('/Shop')
+     else if(z==="About")navigate('/About')
+       else if(z==="Contact")navigate('/Contact')
+  }
+
   return (
     <>
       <div className="footer">
-      <Divider />
-        <Row > 
+        <Divider />
+        <Row >
           <Col span={8}>
             <div className="footer-1">
               <Row>
@@ -14,7 +23,7 @@ function Footer() {
                     Furniro.
                   </span>
                 </Col>
-                <Col style={{color:"#9F9F9F" , fontSize:"1rem"}}>
+                <Col style={{ color: "#9F9F9F", fontSize: "1rem" }}>
                   <p>400 University Drive Suite 200 Coral </p>
                   <p>Gables,</p>
                   <p>FL 33134 USA</p>
@@ -25,17 +34,17 @@ function Footer() {
           <Col span={16}>
             <div className="footer-2">
               <div className="f2-a">
-                <ul  style={{listStyle:"none"}}>
-                  <li>Links</li>
-                <li>Home</li>
-                  <li>Shop</li>
-                  <li>About</li>
-                  <li>Contact</li>
+                <ul style={{ listStyle: "none" }}>
+                  <li style={{cursor:"default"}}>Links</li>
+                  <li onClick={() => handleClick("Home")}>Home</li>
+                  <li onClick={() => handleClick("Shop")}>Shop</li>
+                  <li onClick={() => handleClick("About")}>About</li>
+                  <li onClick={() => handleClick("Contact")}>Contact</li>
                 </ul>
               </div>
-              
+
               <div className="f2-b">
-                <ul style={{listStyle:"none"}}>
+                <ul style={{ listStyle: "none" }}>
                   <li>Help</li>
                   <li>Payment Options</li>
                   <li>Returns</li>
@@ -44,22 +53,22 @@ function Footer() {
               </div>
 
               <div className="f2-c">
-                <ul style={{listStyle:"none"}}>
+                <ul style={{ listStyle: "none" }}>
                   <li>Newsletter</li>
                   <li><input type="email" placeholder="Enter Your Email Address"></input>
-                <button type="button">SUBSCRIBE</button>
-                </li>
+                    <button type="button">SUBSCRIBE</button>
+                  </li>
                 </ul>
-                
-                
+
+
               </div>
             </div>
           </Col>
         </Row>
-<Divider/>
-<footer>2023 furino. All rights reserved</footer>
+        <Divider />
+        <footer>2023 furino. All rights reserved</footer>
       </div>
-    
+
     </>
   );
 }
